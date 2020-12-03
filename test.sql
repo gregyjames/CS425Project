864a4abc3b9597d56cfd90c9a22e79866c902713
Restyled.io <commits@restyled.io>
2020-12-03 21:58:21 +0000

    Restyled by whitespace

--- test.sql
+++ test.sql
@@ -59,4 +59,3 @@ CREATE TABLE `login` (
     `login_time` DATETIME NOT NULL,
     `logout_time` DATETIME NOT NULL,
     PRIMARY KEY (`id`)) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
-
